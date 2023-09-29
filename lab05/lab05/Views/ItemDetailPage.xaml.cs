77befb08a415f4bb13f98127f5b4913c95ed1269
Alumno <Alumno@LAB1509-31>
2023-09-29 15:00:48 -0500

    Agregar archivos de proyecto.

---
+++ lab05/lab05/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using lab05.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace lab05.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ lab05/lab05/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using lab05.Models;
+using lab05.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lab05.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
